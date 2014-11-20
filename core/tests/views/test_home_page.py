60df39e5613909a678905649821395de46b43f88
Jainaldo Silva <jainaldosilva@gmail.com>
2014-11-20 21:18:21 -0200

    Test ultilizando should

--- core/tests/views/test_home_page.py
+++ core/tests/views/test_home_page.py
@@ -2,6 +2,7 @@
 from django.test import TestCase
 from model_mommy import mommy
 from django.core.urlresolvers import reverse
+from should_dsl import should
 
 
 class TestViewHome(TestCase):
@@ -14,13 +15,13 @@ class TestViewHome(TestCase):
 
     def test_homepage(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        response.status_code |should| equal_to(200)
 
     def test_template_usado_seja_home_page_html(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, "core/home_page.html")
+        response.template_name |should| equal_to(['core/home_page.html'])
 
     def test_check_se_contem_url_para_ver_os_fundos(self):
         response = self.client.get('/')
         fundos_url = reverse('fundo')
-        self.assertIn(fundos_url, response.content)
+        fundos_url |should| be_into(response.content)
