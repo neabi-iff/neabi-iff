05fc73a681e0ef34e9c6c21fb265dd9536c5b272
Jainaldo <jainaldosilva@gmail.com>
2014-12-04 18:19:22 -0200

    Configuração para postar noticias no blog neabi

--- neabi/settings.py
+++ neabi/settings.py
@@ -26,16 +26,24 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_comments',
+    'tagging',
+    'mptt',
+    'zinnia',
+    'tinymce',
+    'zinnia_tinymce',
     'django_nose',
     'bootstrap3',
     'core',
@@ -96,6 +104,7 @@ MEDIA_URL = '/uploads/'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
     'hamlpy.template.loaders.HamlPyFilesystemLoader',
     'hamlpy.template.loaders.HamlPyAppDirectoriesLoader',
 )
@@ -104,12 +113,44 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates/'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'zinnia.context_processors.version',
+)
+
 BOOTSTRAP3 = {
     'jquery_url':os.path.join(STATIC_URL,'js/jquery-1.11.1.min.js'),
     'css_url': os.path.join(STATIC_URL,'bootstrap3/css/bootstrap.min.css'),
     'javascript_url':os.path.join(STATIC_URL,'bootstrap3/js/bootstrap.min.js'),
     'horizontal_label_class': 'col-md-3',
     'horizontal_field_class': 'col-md-8',
+}
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme' : "advanced",
+    'plugins' : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
+ 
+    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,code",
+    'theme_advanced_buttons2' : "cut,copy,paste,pastetext,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,forecolor,backcolor",
+    'theme_advanced_buttons3' : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,advhr,|,ltr,rtl",
+ 
+    'theme_advanced_toolbar_location' : "top",
+    'theme_advanced_toolbar_align' : "left",
+    'theme_advanced_statusbar_location' : "bottom",
+    'theme_advanced_resizing' : 'true',
+ 
+ 
+    'template_external_list_url' : "lists/template_list.js",
+    'external_link_list_url' : "lists/link_list.js",
+    'external_image_list_url' : "lists/image_list.js",
+    'media_external_list_url' : "lists/media_list.js",
+ 
 }
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
--- neabi/urls.py
+++ neabi/urls.py
@@ -8,8 +8,13 @@ from core.views import HomePageView
 urlpatterns = patterns('',
     url(r'^$', HomePageView.as_view()),
     url(r'^fundo/', include('fundo.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^neabi/', include('zinnia.urls', namespace = 'zinnia')),
+    url(r'^comments/', include('django_comments.urls')),
+    url(r'^tinymce/zinnia/', include('zinnia_tinymce.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    (r'^tinymce/', include('tinymce.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
