136a2401a8572484a6eb67101dfb2396ae979480
Jainaldo Silva <jainaldosilva@gmail.com>
2015-02-11 21:38:24 -0200

    alterando database novamente

--- neabi/settings.py
+++ neabi/settings.py
@@ -72,21 +72,20 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-# import dj_database_url
-# DATABASES = {'default': dj_database_url.config(default='postgres://qgdouegqqwmija:Ir6tVo2KrDPRf4JFkFWnxC1Rlu@ec2-54-83-43-49.compute-1.amazonaws.com:5432/daobm27ucojnui') }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'daobm27ucojnui',
-#         'USER': 'qgdouegqqwmija',
-#         'PASSWORD':'Ir6tVo2KrDPRf4JFkFWnxC1Rlu',
-#         'HOST': 'ec2-54-83-43-49.compute-1.amazonaws.com',
-#         'PORT':'5432'
-#     }
-# }
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://kezaprpdjkhcsd:IzStmcXCDufj9K1naDEmdre6Lz@ec2-54-228-195-37.eu-west-1.compute.amazonaws.com:5432/d8fbam87jc7cqm') }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8fbam87jc7cqm',
+        'USER': 'kezaprpdjkhcsd',
+        'PASSWORD':'IzStmcXCDufj9K1naDEmdre6Lz',
+        'HOST': 'ec2-54-228-195-37.eu-west-1.compute.amazonaws.com',
+        'PORT':'5432'
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
