ad7a1765b2a6a085845fcbb2a6e921d998ed1b15
Rayssa Bugine <rayssa.rbugine@gmail.com>
2015-05-04 15:26:06 -0300

    Criada views para Acervo, Neabi e Contato.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from fundo.models import Fundo
+from core.models import Contato, Acervo, SobreNeabi
 
 
 class HomePageView(TemplateView):
@@ -10,3 +11,18 @@ class HomePageView(TemplateView):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['fundos'] = Fundo.objects.all()
         return context
+
+
+class Contato(ListView):
+    model = Contato
+    template_name = "core/contato_list.html"
+
+
+class Acervo(ListView):
+    model = Acervo
+    template_name = "core/acervo_list.html"
+
+
+class SobreNeabi(ListView):
+    model = SobreNeabi
+    template_name = "core/sobre_neabi_list.html"
