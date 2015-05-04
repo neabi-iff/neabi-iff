324aeaeca870acbeb3b814b0e2e421c383738027
Rayssa Bugine <rayssa.rbugine@gmail.com>
2015-05-04 15:24:04 -0300

    Criado urls para as views.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Contato(models.Model):
+    descricao = models.TextField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    facebook = models.URLField()
+
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
+    def __unicode__(self):
+        return '%s' % (self.descricao)
+
+    # def save(self, *args, **kwargs):
+    #     if Contato.objects.count() == 0:
+    #         return super(Contato, self).save(*args, **kwargs)
+
+
+class Acervo(models.Model):
+    nome = models.CharField(max_length=1024)
+    imagem = models.ImageField()
+    link = models.URLField()
+
+    class Meta:
+        verbose_name = 'Acervo'
+        verbose_name_plural = 'Acervos'
+
+    def __unicode__(self):
+        return '%s' % (self.nome)
+
+
+class SobreNeabi(models.Model):
+    sobre = models.TextField()
+
+    class Meta:
+        verbose_name = 'Sobre o Neabi'
+        verbose_name_plural = 'Sobre o Neabi'
+
+    def __unicode__(self):
+        return '%s' % (self.sobre)
--- neabi/urls.py
+++ neabi/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 from core.views import HomePageView
+from core.views import Contato, Acervo, SobreNeabi
 
 urlpatterns = patterns('',
     url(r'^$', HomePageView.as_view()),
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
     url(r'^neabi/', include('zinnia.urls', namespace = 'zinnia')),
     url(r'^comments/', include('django_comments.urls')),
     url(r'^tinymce/zinnia/', include('zinnia_tinymce.urls')),
+    url(r'^contato/$', Contato.as_view(), name='contato'),
+    url(r'^acervo/$', Acervo.as_view(), name='acervo'),
+    url(r'^neabi/sobre/$', SobreNeabi.as_view(), name='sobre_neabi'),
 
     url(r'^admin/', include(admin.site.urls)),
     (r'^tinymce/', include('tinymce.urls')),
